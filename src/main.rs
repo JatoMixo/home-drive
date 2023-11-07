016f423c1e5187c24af25571add0fdef843a1065
JatoMixo <jatomixo@outlook.es>
2023-11-07 16:43:28 +0100

    Finished serving the storagement route to make downloading files available

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-use tide::Request;
+async fn handle_download_route(app: &mut tide::Server<()>) -> tide::Result<()> {
+    const ROUTE: &str = "/download";
+    const DIR_NAME: &str = "drive-storagement";
+
+    app.at(ROUTE).serve_dir(DIR_NAME)?;
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> tide::Result<()> {
     femme::start();
 
     let mut app = tide::new();
+
+    handle_download_route(&mut app).await?;
 
     app.listen("0.0.0.0:8080").await?;
 
