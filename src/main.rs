1170fc7b89a0fa35198661c769905c3f443a80ce
JatoMixo <jatomixo@outlook.es>
2023-11-26 11:45:34 +0100

    Discovered how to do infinite routing for explorer

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 mod download;
 use download::handle_download_route;
 
+async fn test(req: tide::Request<()>) -> tide::Result<String> {
+    let file_path = req.url().path();
+
+    Ok(file_path.to_string())
+}
+
 #[tokio::main]
 async fn main() -> tide::Result<()> {
     femme::start();
 
     let mut app = tide::new();
-    app.at("/").get(|_| async{ Ok("Welcome to the Home Drive") });
 
     handle_download_route(&mut app).await?;
+    app.at("/*").get(test);
 
     app.listen("0.0.0.0:8080").await?;
 
