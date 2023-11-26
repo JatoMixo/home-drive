8005063580395631288a1a850afcf949d19365db
JatoMixo <jatomixo@outlook.es>
2023-11-26 17:47:57 +0100

    Made the explorer work

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 mod download;
 use download::handle_download_route;
+use std::fs::read_dir;
 
-async fn test(req: tide::Request<()>) -> tide::Result<String> {
+async fn explorer(req: tide::Request<()>) -> tide::Result<tide::Body> {
     let file_path = req.url().path();
+    let file_list = read_dir(format!("drive-storagement/{}", file_path)).unwrap().map(|file| {
+        String::from(file.unwrap().path().file_name().unwrap().to_str().unwrap())
+    }).collect::<Vec<String>>();
 
-    Ok(file_path.to_string())
+    Ok(tide::Body::from_json(&file_list)?)
 }
 
 #[tokio::main]
@@ -14,7 +18,7 @@ async fn main() -> tide::Result<()> {
     let mut app = tide::new();
 
     handle_download_route(&mut app).await?;
-    app.at("/*").get(test);
+    app.at("/*").get(explorer);
 
     app.listen("0.0.0.0:8080").await?;
 
