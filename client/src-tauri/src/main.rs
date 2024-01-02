f94cdbf6c040b63718e9efc27ae122a8ba0b215b
JatoMixo <jatomixo@outlook.es>
2024-01-02 17:04:16 +0100

    Deleted HTTP File from Rust code; Going to do it from Javascript

--- client/src-tauri/src/main.rs
+++ client/src-tauri/src/main.rs
@@ -1,13 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod http;
-use http::http_get;
-
 fn main() {
-
-    println!("{}", http_get("http://localhost:8080//"));
-
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
