6d76428b87253e1ef746fa81f4dac6c60764fe88
JatoMixo <jatomixo@outlook.es>
2024-01-04 20:34:10 +0100

    Server now returns objects when listing directories

--- server/src/explorer.rs
+++ server/src/explorer.rs
@@ -1,9 +1,9 @@
-use crate::file_manager::list_elements_in_directory;
+use crate::file_manager::file_elements_in_path;
 
 pub async fn handle_explorer(req: tide::Request<()>) -> tide::Result<tide::Body> {
-    let file_path = req.url().path();
+    let file_path = req.url().path().to_string();
 
-    let file_list = list_elements_in_directory(file_path);
+    let file_list = file_elements_in_path(file_path);
 
     Ok(tide::Body::from_json(&file_list)?)
 }
--- server/src/file_manager.rs
+++ server/src/file_manager.rs
@@ -1,7 +1,37 @@
 pub const STORAGEMENT_DIR_NAME: &str = "drive-storagement";
 use std::fs::read_dir;
 
-pub fn list_elements_in_directory(file_path: &str) -> Vec<String> {
+#[derive(serde::Serialize)]
+pub struct FileElement {
+    pub name: String,
+    pub is_directory: bool,
+}
+
+impl FileElement {
+    pub fn new(name: String, is_directory: bool) -> FileElement {
+        FileElement {
+            name: name,
+            is_directory: is_directory,
+        }
+    }
+}
+
+pub fn file_elements_in_path(path: String) -> Vec<FileElement> {
+    string_vector_to_file_elements(list_elements_in_directory(path))
+}
+
+fn string_vector_to_file_elements(string_vector: Vec<String>) -> Vec<FileElement> {
+    string_vector
+        .iter()
+        .map(|string| {
+            let is_directory = !string.contains(".");
+
+            FileElement::new(string.to_string(), is_directory)
+        })
+        .collect::<Vec<FileElement>>()
+}
+
+fn list_elements_in_directory(file_path: String) -> Vec<String> {
     read_dir(format!("{}/{}", STORAGEMENT_DIR_NAME, file_path)).unwrap().map(|file| {
         String::from(file.unwrap()
             .path()
