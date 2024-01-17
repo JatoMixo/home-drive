313e9a0175e4af9120ba268c35204e8ad6361b7a
JatoMixo <jatomixo@outlook.es>
2024-01-17 19:31:14 +0100

    Removed useless if in delete route

--- server/src/routes/delete.rs
+++ server/src/routes/delete.rs
@@ -3,12 +3,7 @@ use crate::file_manager::{Directory, File};
 pub async fn handle_delete_route(req: tide::Request<()>) -> tide::Result<String> {
     let path = req.query::<Directory>()?.get_path();
 
-    // Check if it's file. Dumb but functional way
-    if path.contains(".") {
-        let _ = File::open(&path).get_deleted();
-        return Ok(String::new());
-    }
-
+    let _ = File::open(&path).get_deleted();
     let _ = Directory::new(&path).get_deleted();
     Ok(String::new())
 }
